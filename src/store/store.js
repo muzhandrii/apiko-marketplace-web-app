1095bd8c1e65e5838ec1ce73d8952304cf6b68c5
Muzh Andriy <muzhandriy@gmail.com>
2019-06-14 00:08:19 +0300

    first commit

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from '../modules';
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk)),
-);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
